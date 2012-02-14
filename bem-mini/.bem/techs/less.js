69b25e7facf2d1eb229fa685cd108a95835bad7f
Alexander Boythenko <banzalik@gmail.com>
2012-02-14 13:00:43 +0200

    первый вариант собирает less | js | HTML Make file

--- bem-mini/.bem/techs/less.js
+++ bem-mini/.bem/techs/less.js
@@ -1,1 +1,12 @@
-exports.baseTechPath = require.resolve('bem/lib/techs/ie.css');
+var myPath = require('bem/lib/path'),
+    Template = require('bem/lib/template'),
+    INHERIT = require('inherit'),
+    CssTech = require('bem/lib/techs/css').Tech;
+
+exports.Tech = INHERIT(CssTech, {
+
+    getBuildResultChunk: function(relPath, path, suffix) {
+        return '@import url("' + relPath + '");\n';
+    }
+
+});
