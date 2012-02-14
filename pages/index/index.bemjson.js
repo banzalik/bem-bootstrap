cf3a56932c5840c04836a2282670df0ceac1a9eb
Alexander Boythenko <banzalik@gmail.com>
2012-02-14 12:50:07 +0200

    промежуточный комит

--- pages/index/index.bemjson.js
+++ pages/index/index.bemjson.js
@@ -3,7 +3,7 @@
     title: 'index',
     head: [
         { elem: 'css', url: 'index.css'},
-        { elem: 'css', url: 'index.ie.css', ie: 'lt IE 8'},
+        { block: 'i-jquery', elem: 'core'},
         { elem: 'js', url:'index.js'}
     ],
     content: [
